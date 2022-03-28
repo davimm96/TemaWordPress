511dbe520a0d460773e757a92a0ddf5256e69456
davimm96 <davimichaelsenm@gmail.com>
2022-03-28 10:57:47 -0300

    commit

--- general-template.php
+++ general-template.php
@@ -1,35 +1,49 @@
-<?php
-//Template name: General Template
+<?php 
+
+/*
+
+Template Name: General Template
+
+*/
+
 ?>
-<?php get_header();//Chama header.php?>
-<div class="content-area">
-    <main>
-        <section class="middle-area">
-        	<div class="container">
-       
-                        <div class="general-template">
-                        <?php
-                        // Se houver algum post
-                        if(have_posts()):
-                            //Enquanto houver posts, mostre-os
-                            while(have_posts()): the_post();
-                        ?>
-                        <article>
-                            <h2><?php the_title(); //Titulo do artigo?></h2>
-                            <?php the_content();?>
-                        </article>
-                   
-                        <?php 
-                            endwhile;
-                        else:
-                        ?>
-                        <p>There´s nothing yet to be displayed...</p>
-                        <?php endif;?>
-                    </div>
-             
-        	</div>
-        </section>
-    </main>
-</div>
-<?php get_footer();//Chama footer.php?>
-   
+
+<?php get_header(); ?>
+
+<img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
+	<div class="content-area">
+		<main>
+			<section class="middle-area">
+				<div class="container">
+
+						<div class="general-template">
+							<?php 
+							// Se houver algum post
+							if( have_posts() ):
+								// Enquanto houver posts, mostre-os pra gente
+								while( have_posts() ): the_post();
+
+							?>
+
+							<article>
+								<h2><?php the_title(); ?></h2>
+								<?php the_content(); ?>
+							</article>
+
+							<?php 
+								endwhile;
+							else:
+							?>
+
+							 <p>There's nothing yet to be displayed...</p>
+
+							<?php endif; ?>
+
+						</div>						
+
+				</div>				
+			</section>
+		</main>
+	</div>
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Chama css e js-->
-    <?php wp_head();?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Curso WordPress</title>
+	<?php wp_head(); ?>
 </head>
-<!-- Adiciona classe ao bady altomaticamente-->
-<body <?php body_class();?>>
-    <p>Este é um parágrafo de referência</p>
-    <header>
-        <section class="top-bar">
-            <div class="container">
-                <div class="row">
-                    <div class="social-media-icons col-xl-9 col-sm-7 col-6">Icones Sociais</div>
-                    <div class="search col-xl-3 col-sm-5 text-right">Pesquisa</div>
-                </div>
-            </div>
-        </section>
-        <section class="menu-area">
-        <div class="container">
-            <div class="row">
-                <section class="logo col-md-2 col-12 text-center">Logo</section>
-		        <nav class="main-menu menu col-md-10 text-right">
-                    <!-- Implementa menu topo, com o slug: my_main_menu-->
-                    <?php wp_nav_menu(array('theme_location' => 'my_main_menu'));?>
-                </nav>
-            </div>
-        </div>
-        </section>
-    </header>
+<body <?php body_class(); ?>>
+	<header>
+		<section class="top-bar">
+			<div class="container">
+				<div class="row">
+					<div class="social-media-icons col-xl-9 col-sm-7 col-6">Ícones Sociais</div>
+					<div class="search col-xl-3 col-sm-5 col-6 text-right">Pesquisa</div>			
+				</div>
+			</div>
+		</section>
+		<section class="menu-area">
+			<div class="container">
+				<div class="row">
+					<section class="logo col-md-2 col-12 text-center">Logo</section>
+					<nav class="main-menu col-md-10 text-right">
+						<?php 
+						wp_nav_menu( 
+							array( 
+								'theme_location' => 'my_main_menu' 
+							) 
+						); 
+						?>
+					</nav>					
+				</div>
+			</div>
+		</section>
+	</header>
